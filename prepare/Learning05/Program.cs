0c652911ce1bbf2a24211c8e0e8a3b091ce50b9d
mscopa <maurocopa20@gmail.com>
2023-07-03 15:18:22 -0300

    Creating a shape program with four classes

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape>();
+
+        Square square = new Square("red", 5);
+        Console.WriteLine(square.GetColor());
+        Console.WriteLine(square.GetArea());
+        shapes.Add(square);
+
+        Rectangle rectangle = new Rectangle("blue", 12, 5);
+        Console.WriteLine(rectangle.GetColor());
+        Console.WriteLine(rectangle.GetArea());
+        shapes.Add(rectangle);
+
+        Circle circle = new Circle("green", 3);
+        Console.WriteLine(circle.GetColor());
+        Console.WriteLine(circle.GetArea());
+        shapes.Add(circle);
+        
+        foreach (Shape shape in shapes)
+        {
+            string color = shape.GetColor();
+            double area = shape.GetArea();
+
+            Console.WriteLine($"The {color} shape has an area of {area}.");
+        }
     }
 }
